e384c2134a9aaa2a0f5dd02f1942acac14ebe89e
geanepatriota <geanepatriota14@gmail.com>
2026-05-14 00:55:44 -0300

    Update croquis.js

--- croquis.js
+++ croquis.js
@@ -1,5 +1,5 @@
 const croquisImages = [
-    'croquis/1.jpg', 'croquis/2.jpg', 'croquis/3.jpg', 'croquis/4.jpg', 'croquis/5.jpg', 'croquis/6.jpg'
+    'croquis/1.jpg', 'croquis/2.jpg', 'croquis/3.jpg', 'croquis/4.jpg', 'croquis/5.jpg', 'croquis/6.jpg', 'croquis/7.jpg', 'croquis/8.jpg', 'croquis/9.jpg', 'croquis/10.jpg', 'croquis/11.jpg', 'croquis/12.jpg', 'croquis/13.jpg', 'croquis/14.jpg', 'croquis/15.jpg', 'croquis/16.jpg', 'croquis/17.jpg', 'croquis/18.jpg', 'croquis/19.jpg', 'croquis/20.jpg', 'croquis/21.jpg', 'croquis/22.jpg', 'croquis/23.jpg'
 ];
 
 function loadCroquis() {
